a0f40326f33312f5a5253854b61f15b7deec1ded
Abhilash107 <abhilash00003108884@gmail.com>
2024-12-04 09:58:44 +0530

    redis

--- string.js
+++ string.js
@@ -2,8 +2,11 @@ import { client } from "./client.js";
 
 async function init() {
     //await client.set('message:5', "Hello")
-    await client.expire('message:5', 10);
-    const res = await client.get('message:5')
+    //await client.expire('message:5', 10);
+    //await client.set("count", 0)
+    await client.decrby("count", 10)
+    const res = await client.get("count")
+   
     console.log(res);
     
 }
